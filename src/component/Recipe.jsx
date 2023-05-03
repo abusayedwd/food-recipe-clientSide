fc65f474c209abe09e3bc9a7d63770b483f0e56b
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-03 18:33:19 +0600

    added to recipe beneer

--- src/component/Recipe.jsx
+++ src/component/Recipe.jsx
@@ -22,20 +22,40 @@ const Recipe = () => {
        
         return (
                 <div>
-                <div className='flex w-3/4 mx-auto gap-8 mt-12'>
-                        <div className='w-4/5'>
-                 <img  className=" h-72 w-full rounded-lg" src= {photo} />
-                       </div>
+                
                  <div>
-              <h1 className='text-2xl font-bold'> Name : {name}</h1> 
-                        <p> <span className='  font-bold'>Some bio</span> : {bio}</p>
-                        <p> <span className='font-bold'>like</span> : {likes}</p>
-                        <p> <span className='font-bold'>Recipe quantity</span> : {number_of_recipes}</p>
-                        <p> <span className='font-bold'>Experience</span> : {experience}</p>
+
+<div className="card card-side bg-base-100 shadow-xl mt-8">
+  <figure><img className='w-full' src= {photo} alt="Movie"/></figure>
+  <div className="card-body w-full">
+  <div  className="hero h-80 w-full" style={{ backgroundImage: `url("/public/images/cardb.jpg")` }}>
+   <div className="hero-overlay bg-opacity-60"></div>
+   <div className="hero-content text-start text-neutral-content">
+     <div className="max-w-md">
+       <h1 className="mb-5 text-5xl font-bold">{name}</h1>
+       <p> <span className='  font-bold'>Some bio</span> : {bio}</p>
+                         <div className='flex mt-12'>
+                         <p className='border rounded-xl border-y-slate-50 p-3 mx-4'> <span className='font-bold'>like</span> : {likes}</p>
+                         <p className='border rounded-xl border-y-slate-50 p-3 mx-4'> <span className='font-bold'>Recipe quantity</span> : {number_of_recipes}</p>
+                         <p className='border rounded-xl border-y-slate-50 p-3 mx-4'> <span className='font-bold'>Experience</span> : {experience}</p>
+                         </div>
+    
+     </div>
+   </div>
+ </div>
+  </div>
+</div>
+
+
+
+ 
+
+
+               
                  </div>
 
-        </div>
-                <h1 className='text-2xl font-bold my-8 text-center'>Three Recipe of Chef:</h1>  
+        
+                <h1 className='text-2xl font-bold my-8 text-center'>Three Recipe of {name}:</h1>  
                 <div className=" grid grid-cols-1 md:grid-cols-3 w-3/4 mx-auto gap-6 mt-8">
                 {
                         recipe.map(r => <Recipecard
