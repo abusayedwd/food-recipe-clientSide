2e0b8444334e587d32887a2ec2e7e2bde69bef6d
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-03 23:20:13 +0600

    rirebase env config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgdEu5OzcawqXgbk8kFwJWUzFhjh7cyOA",
-  authDomain: "food-recipe-a10.firebaseapp.com",
-  projectId: "food-recipe-a10",
-  storageBucket: "food-recipe-a10.appspot.com",
-  messagingSenderId: "280065846093",
-  appId: "1:280065846093:web:c86e819519225b0923a504"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
